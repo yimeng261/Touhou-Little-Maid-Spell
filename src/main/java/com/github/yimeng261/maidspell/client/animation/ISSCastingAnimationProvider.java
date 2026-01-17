38442d384f505b7111a830b2391dd18bf6bd2be0
Gardel <gardel741@outlook.com>
2026-01-17 23:46:49 +0800

    fix: 修复施法动作问题

--- src/main/java/com/github/yimeng261/maidspell/client/animation/ISSCastingAnimationProvider.java
+++ src/main/java/com/github/yimeng261/maidspell/client/animation/ISSCastingAnimationProvider.java
@@ -3,7 +3,12 @@ package com.github.yimeng261.maidspell.client.animation;
 import com.github.tartaricacid.touhoulittlemaid.api.animation.IMagicCastingAnimationProvider;
 import com.github.tartaricacid.touhoulittlemaid.api.animation.IMagicCastingState;
 import com.github.tartaricacid.touhoulittlemaid.api.entity.IMaid;
+import com.github.tartaricacid.touhoulittlemaid.client.resource.CustomPackLoader;
+import com.github.tartaricacid.touhoulittlemaid.client.resource.pojo.MaidModelInfo;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.core.builder.AnimationBuilder;
+import com.github.tartaricacid.touhoulittlemaid.geckolib3.core.builder.ILoopType;
+import com.github.tartaricacid.touhoulittlemaid.geckolib3.file.AnimationFile;
+import com.github.tartaricacid.touhoulittlemaid.geckolib3.resource.GeckoLibCache;
 import com.github.yimeng261.maidspell.client.spell.CastingAnimateStateAccessor;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
@@ -37,17 +42,13 @@ public class ISSCastingAnimationProvider implements IMagicCastingAnimationProvid
         if (!(state instanceof MagicCastingAnimateState animateState)) {
             return null;
         }
-        if (animateState.getCurrentPhase() == IMagicCastingState.CastingPhase.START
-                || animateState.getCurrentPhase() == IMagicCastingState.CastingPhase.CASTING
-                || animateState.getCurrentPhase() == IMagicCastingState.CastingPhase.INSTANT) {
-            return getStartAnimationFromSpell(animateState);
-        } else if (animateState.getCurrentPhase() == IMagicCastingState.CastingPhase.END) {
-            return getFinishAnimationFromSpell(animateState);
+        IMagicCastingState.CastingPhase currentPhase = animateState.getCurrentPhase();
+        if (currentPhase == null || currentPhase == IMagicCastingState.CastingPhase.NONE) {
+            return null;
         }
-        return null;
-    }
 
-    private AnimationBuilder getStartAnimationFromSpell(MagicCastingAnimateState animateState) {
+        MaidModelInfo maidModelInfo = CustomPackLoader.MAID_MODELS.getInfo(maid.getModelId()).orElse(null);
+        AnimationFile animationFile = maidModelInfo == null ? null : GeckoLibCache.getInstance().getAnimations().get(maidModelInfo.getModelId());
         SpellData castingSpell = animateState.getCastingSpell();
         AbstractSpell spell;
         if (castingSpell == null || castingSpell.getSpell() == SpellRegistry.none()) {
@@ -58,19 +59,21 @@ public class ISSCastingAnimationProvider implements IMagicCastingAnimationProvid
         if (spell == null || spell == SpellRegistry.none()) {
             return null;
         }
+        if (currentPhase == IMagicCastingState.CastingPhase.START
+                || currentPhase == IMagicCastingState.CastingPhase.CASTING
+                || currentPhase == IMagicCastingState.CastingPhase.INSTANT) {
+            return getStartAnimationFromSpell(animationFile, spell, animateState);
+        } else if (currentPhase == IMagicCastingState.CastingPhase.END) {
+            return getFinishAnimationFromSpell(animationFile, spell, animateState);
+        }
+        return null;
+    }
+
+    private AnimationBuilder getStartAnimationFromSpell(AnimationFile animationFile, AbstractSpell spell, MagicCastingAnimateState animateState) {
         Optional<RawAnimation> opRawAnimation = spell.getCastStartAnimation().getForMob();
         if (opRawAnimation.isPresent()) {
             RawAnimation rawAnimation = opRawAnimation.get();
-            AnimationBuilder builder = new AnimationBuilder();
-            for (RawAnimation.Stage animationStage : rawAnimation.getAnimationStages()) {
-                if (animationStage.loopType() == Animation.LoopType.LOOP) {
-                    builder.loop(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                } else if (animationStage.loopType() == Animation.LoopType.PLAY_ONCE) {
-                    builder.playOnce(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                } else if (animationStage.loopType() == Animation.LoopType.HOLD_ON_LAST_FRAME) {
-                    builder.playAndHold(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                }
-            }
+            AnimationBuilder builder = toTlmAnimation(animationFile, rawAnimation);
             animateState.setCancelled(false);
             if (spell.getCastType() == CastType.INSTANT) {
                 animateState.clearInstantCastSpellType();
@@ -82,30 +85,43 @@ public class ISSCastingAnimationProvider implements IMagicCastingAnimationProvid
         }
     }
 
-    private AnimationBuilder getFinishAnimationFromSpell(MagicCastingAnimateState animateState) {
-        AbstractSpell spell = animateState.getCastingSpell().getSpell();
-        Optional<RawAnimation> opRawAnimation = spell.getCastFinishAnimation().getForMob();
+    private AnimationBuilder getFinishAnimationFromSpell(AnimationFile animationFile, AbstractSpell spell, MagicCastingAnimateState animateState) {
+        animateState.clearInstantCastSpellType();
         if (spell.getCastFinishAnimation().isPass) {
             animateState.setCancelled(false);
             return null;
         }
+        Optional<RawAnimation> opRawAnimation = spell.getCastFinishAnimation().getForMob();
         if (opRawAnimation.isPresent()) {
             RawAnimation rawAnimation = opRawAnimation.get();
-            AnimationBuilder builder = new AnimationBuilder();
-            for (RawAnimation.Stage animationStage : rawAnimation.getAnimationStages()) {
-                if (animationStage.loopType() == Animation.LoopType.LOOP) {
-                    builder.loop(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                } else if (animationStage.loopType() == Animation.LoopType.PLAY_ONCE) {
-                    builder.playOnce(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                } else if (animationStage.loopType() == Animation.LoopType.HOLD_ON_LAST_FRAME) {
-                    builder.playAndHold(ANIMATION_NAME_PREFIX + animationStage.animationName());
-                }
-            }
+            AnimationBuilder builder = toTlmAnimation(animationFile, rawAnimation);
             animateState.setCancelled(false);
             return builder;
         } else {
             animateState.setCancelled(true);
             return null;
         }
+    }
+
+    private static AnimationBuilder toTlmAnimation(AnimationFile animationFile, RawAnimation rawAnimation) {
+        AnimationBuilder builder = new AnimationBuilder();
+        for (RawAnimation.Stage animationStage : rawAnimation.getAnimationStages()) {
+            String animationName = ANIMATION_NAME_PREFIX + animationStage.animationName();
+            com.github.tartaricacid.touhoulittlemaid.geckolib3.core.builder.Animation customAnimation = animationFile.getAnimation(animationName);
+            ILoopType loopType = null;
+            if (customAnimation != null) {
+                loopType = customAnimation.loop;
+            } else {
+                if (animationStage.loopType() == Animation.LoopType.LOOP) {
+                    loopType = ILoopType.EDefaultLoopTypes.LOOP;
+                } else if (animationStage.loopType() == Animation.LoopType.PLAY_ONCE) {
+                    loopType = ILoopType.EDefaultLoopTypes.PLAY_ONCE;
+                } else if (animationStage.loopType() == Animation.LoopType.HOLD_ON_LAST_FRAME) {
+                    loopType = ILoopType.EDefaultLoopTypes.HOLD_ON_LAST_FRAME;
+                }
+            }
+            builder.addAnimation(animationName, loopType);
+        }
+        return builder;
     }
 }
