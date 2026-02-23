2e27a330872c9db845f816abdd45dc83799c6095
Gardel <gardel741@outlook.com>
2026-02-23 14:51:43 +0800

    feat: 鬼厄巫法兼容

---
+++ src/main/java/com/github/yimeng261/maidspell/mixin/MobUtilMixin.java
@@ -0,0 +1,36 @@
+package com.github.yimeng261.maidspell.mixin;
+
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.github.yimeng261.maidspell.spell.data.MaidGoetySpellData;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import za.co.infernos.goety.utils.MobUtil;
+
+/**
+ * Mixin到Goety的MobUtil类
+ * 扩展isSpellCasting方法以支持女仆的施法状态检测
+ * 注意：使用@Pseudo注解表示这个类可能不存在（当Goety未安装时）
+ */
+@Pseudo
+@Mixin(value = MobUtil.class)
+public class MobUtilMixin {
+
+    /**
+     * 在isSpellCasting方法开始时注入
+     * 如果是女仆且正在施放Goety法术，直接返回true
+     */
+    @Inject(method = "isSpellCasting", at = @At("HEAD"), cancellable = true)
+    private static void onIsSpellCasting(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
+        if (livingEntity instanceof EntityMaid maid) {
+            //Global.LOGGER.debug("maid: {}, isCasting:{}", maid.getDisplayName(), MaidGoetySpellData.getOrCreate(maid).isCasting());
+            if (MaidGoetySpellData.getOrCreate(maid).isCasting()) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
+
