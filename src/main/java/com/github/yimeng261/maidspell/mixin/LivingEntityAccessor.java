7e777ca9143e62e59981561d5f5fe2ba30e6e580
Gardel <gardel741@outlook.com>
2026-04-09 02:46:58 +0800

    fix: 修复 SynchedEntityDataMixin 问题

---
+++ src/main/java/com/github/yimeng261/maidspell/mixin/LivingEntityAccessor.java
@@ -0,0 +1,12 @@
+package com.github.yimeng261.maidspell.mixin;
+
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Accessor("DATA_HEALTH_ID")
+    EntityDataAccessor<Float> getDataHealthIdAccessor();
+}
